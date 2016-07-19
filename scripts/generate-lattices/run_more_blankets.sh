c38c75d4e6643798af280dd959154fbca4fb3d52
Ondrej Chvala <ondrejch@gmail.com>
2016-07-19 16:43:10 -0400

    run_more_blankets.sh working

--- scripts/generate-lattices/run_more_blankets.sh
+++ scripts/generate-lattices/run_more_blankets.sh
@@ -7,8 +7,8 @@
 
 OLDJOBDIR="/home/ondrejch/msbr-scan/job1"           # Original job directory
 NEWJOBDIR="/home/ondrejch/msbr-scan/job1_1"         # Here we will run new jobs
-QSUB="/home/ondrejch/msbr-scan/scripts/qsub.sh"     # Command to qsub the job
-DECKWRITER="/home/o/research/Flibe/MSBR-ORNL-4528/scripts/generate-lattices/write-msbr-in_dir.py"
+# QSUB="/home/ondrejch/msbr-scan/scripts/qsub.sh"     # Command to qsub the job
+DECKWRITER="/home/ondrejch/msbr-scan/MSBR-ORNL-4528/scripts/generate-lattices/write-msbr-in_dir.py"
 newRelBA="1.220 1.240 1.260 1.280 1.300 1.320"      # New relBAs to run
 
 SAVEDIR=$(pwd)
@@ -17,11 +17,16 @@ for d in $(find . -type d -name "gr*" )    # List of lattices to process
 do 
     echo "Running cases for: $d"
     NEWBASE=${NEWJOBDIR}/$d
-    echo $NEWBASE
-    for relBA in newRelBA
-#    do 
-#       rm qsub.sh.*
-#       qsub $QSUB
-#    done 
+#    echo $NEWBASE
+    for relBA in $newRelBA
+    do 
+        DECKDIR=${NEWBASE}/$relBA
+        echo  $DECKDIR
+        mkdir -p $DECKDIR
+        cd $DECKDIR
+        $DECKWRITER
+    done 
 done
 
+
+cd $SAVEDIR
