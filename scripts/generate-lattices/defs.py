952f03a1317d8c94e3d18b3732fb07f16314e903
Ondrej Chvala <ondrejch@gmail.com>
2016-07-22 03:11:37 -0400

    fixed number of decimal places

--- scripts/generate-lattices/defs.py
+++ scripts/generate-lattices/defs.py
@@ -12,12 +12,11 @@ jobdir            = "/home/ondrejch/msbr-scan/jobX/"		                    # Wher
 filename          = "msbr.inp"		        # Deck file name
 xsectionlibrary   = "09c"			# Cross section library 
 
-mcruns          = 20                            # How many runs per hex size choise
-apothems        = np.arange( 16.5, 20.1,  0.5)  # Hex sizes to scan [cm *10]
+apothems        = np.arange( 6.0, 20.1,  0.5)   # Hex sizes to scan [cm *10]
 saltfractions   = np.arange(0.04, 0.20 ,0.01) 	# FSFs in percent
 blanketfraction = 1.06923                       # Blanket area over one fuel salt ring area used for slit size calculation
-relblanketA     = np.linspace(0.8, 1.2, 21) 	# Blanket salt areas to probe relative to the predicted fraction above
-
+relblanketA     = np.linspace(0.8, 1.3, 31) 	# Blanket salt areas to probe relative to the predicted fraction above
+r2sizes         = np.linspace(0.0, 1.0 ,21)     # Sampling of r2
 
 if __name__ == '__main__':
 	print ("This module contains contants for MSR lattice parameter scan.")
