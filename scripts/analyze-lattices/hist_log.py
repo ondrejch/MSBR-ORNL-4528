43febc7b7c05c0d802b08c0134d5fabc43c7ea5a
Ondrej Chvala <ondrejch@gmail.com>
2016-07-22 13:14:13 -0400

    useful pieces from ~/.python_history

---
+++ scripts/analyze-lattices/hist_log.py
@@ -0,0 +1,33 @@
+from initlattices import LatticeList
+msbrs = LatticeList()
+msbrs.read_data("/home/ondrejch/msbr-scan/job1/testdata")
+msbrs.fit_all_lattices()
+msbrs.latlist[23].plot()
+
+
+from initlattices import LatticeList
+msbrs = LatticeList()
+msbrs.read_data("/home/ondrejch/msbr-scan/job1/sorted.j1.data")
+msbrs.fit_all_lattices()
+
+import pickle
+fout = open("latdump.pickle","wb")
+pickle.dump(msbrs,fout)
+
+
+import pickle
+fin = open("run1_nlat_8938.pickle","rb")
+msbrs=pickle.load(fin)
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+crarr = np.linspace(0.9, 1.0, 11)   
+np.meshgrid(crarr, msbrs.larr, msbrs.sfarr)
+
+
+https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.interpolate.griddata.html
+
+from kmax import MaxKforCR
+mk = MaxKforCR("run1_nlat_8938.pickle")
