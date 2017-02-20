149670f126bc80b422b79e4136a8ab58931d2015
ray830305 <hchang13@vols.utk.edu>
2017-02-20 15:08:19 -0500

    Add files via upload

--- economic_model/plant_cost_model_turbine&salt.py
+++ economic_model/plant_cost_model_turbine&salt.py
@@ -1,22 +1,26 @@
 import os
 import numpy as np
-salt_opt = int(input('Enter the type of salt you want to use: '))
-salt_matrix = [['  salt type', ' index', ' composition', '  price [kg/m^3]'],
-               ['LiF-BeF2-UF4', '  1', '  .63-.366-.004    ',       3774.6],
-               ['LiF-BeF2-UF4', '  2', '  .636-.362-.0022  ',    1936.95],
-               ['LiF-BeF2-UF4', '  3', '  .636-.366-.004    ',     869.4],
-               ['LiF-BeF2-UF4', '  4','   .67-.18-.15      ',        522.4],
-               ['LiF-BeF2-ThF4', ' 5','    .71-.02-.027     ',       2324],
-               ['LiF-BeF2-ThF4', ' 6','    .63-.366-.004  ',    11776.44],
-               ['LiF-BeF2-ThF4', ' 7','    .63-.366-.004    ',      34.37],
-               ['LiF-BeF2-ThF4', ' 8','     .389-.611       ',          709.89],
-               ['  NaBF4-NaF', '   S9', '      .92-.08         ',        2068],
-               ['  NaBF4-NaF', '  10', '     .389-.611       '   ,         2095]]
 
-os.linesep
-
+print ('SALT TYPE TABLE')
+print ('')
+salt_matrix = [['  Salt Type', ' Index', ' Composition', '  Price [$/m^3]'],
+               ['                                                         '],
+               ['LiF-BeF2-UF4' '      1' '      .63-.366-.004    ',       3774.6],
+               ['LiF-BeF2-UF4' '      2' '      .636-.362-.0022  ',    1936.95],
+               ['LiF-BeF2-UF4' '      3' '      .636-.366-.004    ',     869.4],
+               ['LiF-BeF2-UF4' '      4''      .67-.18-.15      ',        522.4],
+               ['LiF-BeF2-ThF4' '     5''      .71-.02-.027     ',       2324],
+               ['LiF-BeF2-ThF4' '     6''      .63-.366-.004  ',    11776.44],
+               ['LiF-BeF2-ThF4' '     7''      .63-.366-.004    ',      34.37],
+               ['LiF-BeF2-ThF4' '     8''      .389-.611       ',          709.89],
+               ['  NaBF4-NaF' '       9' '      .92-.08         ',        2068],
+               ['  NaBF4-NaF' '       10' '     .389-.611       '   ,         2095]]
 
 print(np.matrix(salt_matrix))
+print ('')
+salt_opt = int(input('Enter the type of salt you want to use: '))
+
+
 
 #salt_price = salt_matrix(int(salt_opt), int(salt_opt+3))
 if salt_opt == 1:
@@ -63,23 +67,33 @@ if salt_opt == 10:
 
 if salt_opt > 10 or salt_opt <1:
     import sys
-    sys.exit("must enter the index that is available in the table")
+    sys.exit("must enter the index that is available in the table(input the index of the salt)")
 
+print ('')
 vol_salt = input('Enter your salt volumn [m^3]')
 tot_salt = salt_price * vol_salt
+print ('')
 
-turbine_matrix = [['index', '  company',      '      model',    '     price [kg/m^3]'],
-                  ['  1',     '    siemens',     '     SST-800     ',        1.55e7],
-                  ['  2',     '    siemens',   '   SST-900       ',       1.85e7],
-                  ['  3',      '    Doosan',      '      MTD50        ',         1.02e6],
-                  ['  4',        '      GE',     '      9F 3-series   ',      1.5e7],
-                  ['  5',        '      GE',     '      9F 5-series   ',      1.75e7],
-                  ['  6',       '     DTEC',      '       GMB2550r     ',      8.5e6]]
+turbine_matrix = [['index' '  company'      '      model',    '     price [kg/m^3]'],
+                  ['  1'     '    siemens'     '     SST-800     ',        1.55e7],
+                  ['  2'     '    siemens'   '   SST-900       ',       1.85e7],
+                  ['  3'      '    Doosan'      '      MTD50        ',         1.02e6],
+                  ['  4'        '      GE'     '      9F 3-series   ',      1.5e7],
+                  ['  5'        '      GE'     '      9F 5-series   ',      1.75e7],
+                  ['  6'       '     DTEC'      '       GMB2550r     ',      8.5e6]]
 
 
+print ('Turbine TABLE')
+print ('')
 print(np.matrix(turbine_matrix))
+print ('')
 
-turbine_opt = int(input('Enter the turbine you desire to use: '))
+turbine_opt = int(input('Enter the turbine you desire to use (input the index of the turbine you want): '))
+
+if turbine_opt >= 7 or turbine_opt <1:
+    import sys
+    sys.exit("must enter the index that is available in the table")
+
 if turbine_opt == 1:
    turbine_price = 1.55e7
    print'turbine price is : ',turbine_price
@@ -104,12 +118,10 @@ if turbine_opt == 6:
    turbine_price = 8.5e6
    print 'turbine price is : ',turbine_price
 
-
-
-
-if turbine_opt > 6 or turbine_opt <1:
-    import sys
-    sys.exit("must enter the index that is available in the table")
-
 tot = turbine_price + tot_salt
 print 'facility cost is ', tot, 'dollars'
+
+
+
+
+
