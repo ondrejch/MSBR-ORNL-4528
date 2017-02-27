771d624841322bab9c135d5f04fe6f8d850c8b2c
ray830305 <hchang13@vols.utk.edu>
2017-02-27 10:57:15 -0500

    Update plant_cost_model_turbine&salt.py

--- economic_model/plant_cost_model_turbine&salt.py
+++ economic_model/plant_cost_model_turbine&salt.py
@@ -7,14 +7,14 @@ salt_matrix = [['  Salt Type', ' Index', ' Composition', '  Price [$/m^3]'],
                ['                                                         '],
                ['LiF-BeF2-UF4' '      1' '      .63-.366-.004    ',       3774.6],
                ['LiF-BeF2-UF4' '      2' '      .636-.362-.0022  ',    1936.95],
-               ['LiF-BeF2-UF4' '      3' '      .636-.366-.004    ',     869.4],
+               ['LiF-BeF2-UF4' '      3' '      .636-.366-.004   ',     869.4],
                ['LiF-BeF2-UF4' '      4''      .67-.18-.15      ',        522.4],
                ['LiF-BeF2-ThF4' '     5''      .71-.02-.027     ',       2324],
-               ['LiF-BeF2-ThF4' '     6''      .63-.366-.004  ',    11776.44],
+               ['LiF-BeF2-ThF4' '     6''      .63-.366-.004    ',    11776.44],
                ['LiF-BeF2-ThF4' '     7''      .63-.366-.004    ',      34.37],
-               ['LiF-BeF2-ThF4' '     8''      .389-.611       ',          709.89],
-               ['  NaBF4-NaF' '       9' '      .92-.08         ',        2068],
-               ['  NaBF4-NaF' '       10' '     .389-.611       '   ,         2095]]
+               ['LiF-BeF2-ThF4' '     8''      .389-.611        ',          709.89],
+               ['  NaBF4-NaF' '       9' '      .92-.08          ',        2068],
+               ['  NaBF4-NaF' '       10' '     .389-.611        '   ,         2095]]
 
 print(np.matrix(salt_matrix))
 print ('')
@@ -74,7 +74,82 @@ vol_salt = input('Enter your salt volumn [m^3]')
 tot_salt = salt_price * vol_salt
 print ('')
 
-turbine_matrix = [['index' '  company'      '      model',    '     price [kg/m^3]'],
+print ('SALT coolant Table')
+print ('')
+salt_matrix = [['  coolant Type', ' Index', ' Composition', '  Price [$/m^3]'],
+               ['AHTR candidate coolant salts            ],                                                  '],
+               ['NaF-KF-ZrF4' '        1' '      4-27-69              ',            11700],
+               ['LiF-NaF-KF' '         2' '      29-12-59             ',          24100],
+               ['LiF-NaF-BeF2' '       3' '      24-46-30             ',        35000],
+               ['LiF-BeF2' '           4''      53-47                ',           52200],
+               ['other industrial salts'],
+               ['NaNO3-NaNO2-KNO3' '   5''      7-48-45              ',            570],
+               ['NaF-NaBF4' '          6''      8-92                 ',              1500],
+               ['LiCL-KCL' '           7''      59-41                ',            1800],
+               ['Very low-vapor coolants               '],
+               ['Pb' '                 8''      1                    ',                 720],
+               [' Pb-Bi' '             9' '      1                    ',             74400],
+               ['  Bi' '               10' '     1                    '   ,                129000]]
+
+print(np.matrix(salt_matrix))
+print ('')
+coolant_opt = int(input('Enter the type of coolant you want to use(input index): '))
+
+
+
+#salt_price = salt_matrix(int(salt_opt), int(salt_opt+3))
+if coolant_opt == 1:
+   coolant_price = 11700
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 2:
+   coolant_price = 24100
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 3:
+   coolant_price = 35000
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 4:
+   coolant_price = 52200
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 5:
+   coolant_price = 570
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 6:
+   coolant_price = 1500
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 7:
+   coolant_price = 1800
+   print 'coolant unit price is : ',coolant_price
+
+if coolant_opt == 8:
+   coolant_price = 720
+   print('coolant unit price is : ',coolant_price)
+
+if coolant_opt == 9:
+   coolant_price = 74400
+   print('coolant unit price is : ',coolant_price)
+
+if coolant_opt == 10:
+   coolant_price = 129000
+   print('coolant unit price is : ',coolant_price)
+
+
+
+if coolant_opt > 10 or coolant_opt <1:
+    import sys
+    sys.exit("must enter the index that is available in the table(input the index of the coolant)")
+
+print ('')
+vol_coolant = input('Enter your coolant volumn [m^3]')
+tot_coolant = coolant_price * vol_coolant
+print ('')
+
+turbine_matrix = [['index' '  company'      '      model',    '     price [$/m^3]'],
                   ['  1'     '    siemens'     '     SST-800     ',        1.55e7],
                   ['  2'     '    siemens'   '   SST-900       ',       1.85e7],
                   ['  3'      '    Doosan'      '      MTD50        ',         1.02e6],
@@ -118,9 +193,14 @@ if turbine_opt == 6:
    turbine_price = 8.5e6
    print 'turbine price is : ',turbine_price
 
-tot = turbine_price + tot_salt
+tot = turbine_price + tot_salt + tot_coolant
 print 'facility cost is ', tot, 'dollars'
 
+
+# put table 18 from the last year(done)
+# hitec boiling pt = 816 deg C
+# resource for the turbine price
+# get generator
 
 
 
